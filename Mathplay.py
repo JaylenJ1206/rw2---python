5e746bb5988aa0a8325987c63613d8298b4b041a
JaylenJ <jaylenjj1@icloud.com>
2025-04-10 00:47:02 +0000

    mathplay

---
+++ Mathplay.py
@@ -0,0 +1,14 @@
+myname = "Jaylen"
+
+mymesg = "isthe greatest."
+
+mynum1 = 7
+
+mynum2 = 4
+
+print(myname + " " + mymesg)
+
+product = mynum1 * mynum2 
+product + str(product)
+
+print("the answer is: " + str(product))
