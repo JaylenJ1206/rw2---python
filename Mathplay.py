24d6529d1280b48c8617fa968ce61f0a17eee115
JaylenJ <jaylenjj1@icloud.com>
2025-04-10 01:09:52 +0000

    meaning

--- Mathplay.py
+++ Mathplay.py
@@ -1,12 +1,15 @@
 myname = "Jaylen"
 
-mymesg = "isthe greatest."
+mymesg = "is the greatest."
+
+print(myname + " " + mymesg)
 
 mynum1 = 7
 
 mynum2 = 4
 
 print(myname + " " + mymesg)
+
 
 product = mynum1 * mynum2 
 product + str(product)
---
+++ meaning.py
@@ -0,0 +1,18 @@
+meaning = 42
+
+print("")
+
+if meaning > 10:
+    print("right on!")
+else:
+    print("not today")
+
+    x = 1
+    y = 20
+    z = 16
+
+if x > 5 and y > 10:
+    print("both are ture")
+else:
+    print("atlest 1 is not true")
+        
