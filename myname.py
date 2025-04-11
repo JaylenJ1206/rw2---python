db9a3bbe24043cd8352f34ba79cebe9f2bf9f16e
JaylenJ <jaylenjj1@icloud.com>
2025-04-11 01:02:14 +0000

    myname

--- myname.py
+++ myname.py
@@ -3,3 +3,15 @@ myname = "jaylen"
 msgyear = "2000"
 
 print(myname + " " + msgyear)
+
+line01 = "*******************"
+line02 = "*      Jaylen     *"
+line03 = "*       2000      *"
+line04 = "*     Johnson     *"
+line05 = "*******************"
+
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line05)
