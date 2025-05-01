55f391638018411f3385fd29b9debc8bfc7cafbe
JaylenJ <jaylenjj1@icloud.com>
2025-05-01 01:33:06 +0000

    hello

---
+++ hello_person.py
@@ -0,0 +1,13 @@
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    description = "provides a personal greeting."
+)
+
+parser.add_argument(
+    "-n",
+    "- -name",
+    metavar="name",
+    required=
+)
